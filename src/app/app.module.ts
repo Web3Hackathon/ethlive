05f4a55cd317584543b1d383caee37c486047d28
AntonioCardenas <antonioalx66@gmail.com>
2022-08-07 03:28:45 -0600

    Fix: Conflict with @AbnerGarcia96  branch
    Branch no sincronizada con master
    
    Co-Authored-By: Abner García <abnerjosgarfer@yahoo.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { GraphqlService } from './services/graphql.service';
-import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,8 +13,6 @@ import { FormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ApolloModule,
-    HttpLinkModule,
     HttpClientModule
   ],
   providers: [
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -3,19 +3,29 @@ import { CommonModule } from '@angular/common';
 import { ChatComponent } from "../chat/chat.component";
 import { InfoComponent } from "../info/info.component";
 import { StreamerComponent } from "../streamer/streamer.component";
+import { GraphqlService } from '../services/graphql.service';
+import { NFTStorageService } from '../services/nftstorage.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [CommonModule , ChatComponent , InfoComponent, StreamerComponent],
+  imports: [CommonModule , ChatComponent , InfoComponent, StreamerComponent , FormsModule],
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss']
 })
-export class MainComponent implements OnInit {
+export class MainComponent {
+  title = 'ethlive';
+  file: any;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private graphQLService: GraphqlService,
+    private NFTStorageService: NFTStorageService
+  ){
+    this.graphQLService.getSales().then(console.log);
   }
 
+  uploadNFT(event: any){
+    this.NFTStorageService.storeNFT(event.target.files[0], event.target.files[0].name, 'Dummy image');
+  }
 }
